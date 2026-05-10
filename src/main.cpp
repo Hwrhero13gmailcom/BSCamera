aaf6187d24c6410b2ff75bbaeb63fb143cf2568e
Z3nith_Scriptz <Hwrhero13@gmail.com>
2026-05-10 19:12:52 -0400

    Refactor camera offset mod implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,112 +1,144 @@
+#include "main.hpp"
 #include "config.hpp"
 
-#include "beatsaber-hook/shared/utils/hooking.hpp"
 #include "beatsaber-hook/shared/utils/logging.hpp"
-#include "bsml/shared/BSML.hpp"
-#include "bsml/shared/BSML-Lite.hpp"
+#include "beatsaber-hook/shared/utils/hooking.hpp"
+#include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
+
 #include "custom-types/shared/register.hpp"
+
+#include "GlobalNamespace/MainCamera.hpp"
 #include "UnityEngine/Camera.hpp"
 #include "UnityEngine/Transform.hpp"
 #include "UnityEngine/Vector3.hpp"
 #include "UnityEngine/Quaternion.hpp"
+
+#include "questui/shared/QuestUI.hpp"
+#include "questui/shared/BeatSaberUI.hpp"
+
 #include "HMUI/ViewController.hpp"
+#include "UnityEngine/GameObject.hpp"
+#include "UnityEngine/RectTransform.hpp"
 
-static modloader::ModInfo modInfo{"CameraOffsetMod", "0.1.0", 0};
-static auto logger = Paper::Logger::WithContext<"CameraOffset">();
+#include <string>
 
-// ─── apply settings ───────────────────────────────────────────────────────────
+static ModInfo modInfo;
+static CameraConfig config;
 
-static void ApplyCameraSettings() {
-    auto cam = UnityEngine::Camera::get_main();
-    if (!cam) return;
-
-    cam->set_fieldOfView(getConfig().FOV.GetValue());
-
-    auto t = cam->get_transform();
-    auto pos = t->get_localPosition();
-    t->set_localPosition({
-        pos.x + getConfig().PosX.GetValue(),
-        pos.y + getConfig().PosY.GetValue(),
-        pos.z + getConfig().PosZ.GetValue()
-    });
-
-    auto rot = t->get_localEulerAngles();
-    t->set_localEulerAngles({
-        rot.x + getConfig().RotX.GetValue(),
-        rot.y + getConfig().RotY.GetValue(),
-        rot.z + getConfig().RotZ.GetValue()
-    });
+Logger& getLogger() {
+    static Logger* logger = new Logger(modInfo);
+    return *logger;
 }
 
-// ─── settings menu ────────────────────────────────────────────────────────────
+// ---- Settings UI ----
 
-static void BuildSettingsUI(HMUI::ViewController* vc, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
+void DidActivate(HMUI::ViewController* self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
     if (!firstActivation) return;
 
-    auto parent = vc->get_gameObject();
+    auto container = QuestUI::BeatSaberUI::CreateScrollableSettingsContainer(self->get_transform());
 
-    BSML::Lite::CreateText(parent->get_transform(), "Camera Offset Settings");
-
-    BSML::Lite::CreateSliderSetting(
-        parent->get_transform(), "FOV", 1.0f, getConfig().FOV.GetValue(), 60.0f, 120.0f,
-        [](float val) { getConfig().FOV.SetValue(val); }
-    );
-
-    BSML::Lite::CreateText(parent->get_transform(), "Position Offset");
-    BSML::Lite::CreateSliderSetting(
-        parent->get_transform(), "X", 0.01f, getConfig().PosX.GetValue(), -0.5f, 0.5f,
-        [](float val) { getConfig().PosX.SetValue(val); }
-    );
-    BSML::Lite::CreateSliderSetting(
-        parent->get_transform(), "Y", 0.01f, getConfig().PosY.GetValue(), -0.5f, 0.5f,
-        [](float val) { getConfig().PosY.SetValue(val); }
-    );
-    BSML::Lite::CreateSliderSetting(
-        parent->get_transform(), "Z", 0.01f, getConfig().PosZ.GetValue(), -0.5f, 0.5f,
-        [](float val) { getConfig().PosZ.SetValue(val); }
+    // X Offset
+    QuestUI::BeatSaberUI::CreateSliderSetting(
+        container->get_transform(),
+        "X Offset", 0.01f, config.xOffset, -2.0f, 2.0f,
+        [](float val) {
+            config.xOffset = val;
+            SaveConfig(config);
+        }
     );
 
-    BSML::Lite::CreateText(parent->get_transform(), "Rotation Offset");
-    BSML::Lite::CreateSliderSetting(
-        parent->get_transform(), "Pitch (X)", 1.0f, getConfig().RotX.GetValue(), -45.0f, 45.0f,
-        [](float val) { getConfig().RotX.SetValue(val); }
-    );
-    BSML::Lite::CreateSliderSetting(
-        parent->get_transform(), "Yaw (Y)", 1.0f, getConfig().RotY.GetValue(), -45.0f, 45.0f,
-        [](float val) { getConfig().RotY.SetValue(val); }
-    );
-    BSML::Lite::CreateSliderSetting(
-        parent->get_transform(), "Roll (Z)", 1.0f, getConfig().RotZ.GetValue(), -45.0f, 45.0f,
-        [](float val) { getConfig().RotZ.SetValue(val); }
+    // Y Offset
+    QuestUI::BeatSaberUI::CreateSliderSetting(
+        container->get_transform(),
+        "Y Offset", 0.01f, config.yOffset, -2.0f, 2.0f,
+        [](float val) {
+            config.yOffset = val;
+            SaveConfig(config);
+        }
     );
 
-    BSML::Lite::CreateUIButton(parent->get_transform(), "Reset to Defaults", []() {
-        getConfig().FOV.SetValue(90.0f);
-        getConfig().PosX.SetValue(0.0f);
-        getConfig().PosY.SetValue(0.0f);
-        getConfig().PosZ.SetValue(0.0f);
-        getConfig().RotX.SetValue(0.0f);
-        getConfig().RotY.SetValue(0.0f);
-        getConfig().RotZ.SetValue(0.0f);
-    });
+    // Z Offset
+    QuestUI::BeatSaberUI::CreateSliderSetting(
+        container->get_transform(),
+        "Z Offset", 0.01f, config.zOffset, -2.0f, 2.0f,
+        [](float val) {
+            config.zOffset = val;
+            SaveConfig(config);
+        }
+    );
+
+    // X Rotation
+    QuestUI::BeatSaberUI::CreateSliderSetting(
+        container->get_transform(),
+        "X Rotation", 1.0f, config.xRotation, -180.0f, 180.0f,
+        [](float val) {
+            config.xRotation = val;
+            SaveConfig(config);
+        }
+    );
+
+    // Y Rotation
+    QuestUI::BeatSaberUI::CreateSliderSetting(
+        container->get_transform(),
+        "Y Rotation", 1.0f, config.yRotation, -180.0f, 180.0f,
+        [](float val) {
+            config.yRotation = val;
+            SaveConfig(config);
+        }
+    );
+
+    // Z Rotation
+    QuestUI::BeatSaberUI::CreateSliderSetting(
+        container->get_transform(),
+        "Z Rotation", 1.0f, config.zRotation, -180.0f, 180.0f,
+        [](float val) {
+            config.zRotation = val;
+            SaveConfig(config);
+        }
+    );
 }
 
-// ─── entry points ─────────────────────────────────────────────────────────────
+// ---- Camera hook ----
 
-extern "C" void setup(CModInfo* info) {
-    *info = modInfo.to_c();
-    getConfig().Init(modInfo);
-    logger.info("CameraOffsetMod setup");
+MAKE_HOOK_MATCH(MainCamera_Awake, &GlobalNamespace::MainCamera::Awake, void, GlobalNamespace::MainCamera* self) {
+    MainCamera_Awake(self);
+
+    auto* cam = self->GetComponent<UnityEngine::Camera*>();
+    if (!cam) return;
+
+    auto* transform = cam->get_transform();
+
+    UnityEngine::Vector3 pos = transform->get_localPosition();
+    pos.x += config.xOffset;
+    pos.y += config.yOffset;
+    pos.z += config.zOffset;
+    transform->set_localPosition(pos);
+
+    UnityEngine::Vector3 rot = transform->get_localEulerAngles();
+    rot.x += config.xRotation;
+    rot.y += config.yRotation;
+    rot.z += config.zRotation;
+    transform->set_localEulerAngles(rot);
 }
 
-extern "C" void late_load() {
+// ---- Entry point ----
+
+extern "C" void setup(ModInfo& info) {
+    info.id      = "CameraOffsetMod";
+    info.version = "0.1.0";
+    modInfo      = info;
+    getLogger().info("Camera Offset Mod setup called");
+}
+
+extern "C" void load() {
     il2cpp_functions::Init();
+    config = LoadConfig();
 
-    BSML::Register::RegisterSettingsMenu(
-        "Camera Offset",
-        BuildSettingsUI,
-        false
-    );
+    getLogger().info("Installing Camera Offset Mod hooks...");
+    INSTALL_HOOK(getLogger(), MainCamera_Awake);
 
-    logger.info("CameraOffsetMod loaded");
+    QuestUI::Init();
+    QuestUI::Register::RegisterModSettingsViewController(modInfo, DidActivate);
+
+    getLogger().info("Camera Offset Mod loaded successfully");
 }
