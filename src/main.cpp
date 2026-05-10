9fbef7adbe8432b06cccc959de74021da4e74433
Z3nith_Scriptz <Hwrhero13@gmail.com>
2026-05-10 19:24:03 -0400

    Refactor main.cpp for modloader and BSML integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,101 +1,50 @@
-#include "main.hpp"
 #include "config.hpp"
 
-#include "beatsaber-hook/shared/utils/logging.hpp"
+#include "scotland2/shared/modloader.h"
 #include "beatsaber-hook/shared/utils/hooking.hpp"
-#include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
-
+#include "beatsaber-hook/shared/utils/logging.hpp"
 #include "custom-types/shared/register.hpp"
 
-#include "GlobalNamespace/MainCamera.hpp"
-#include "UnityEngine/Camera.hpp"
-#include "UnityEngine/Transform.hpp"
-#include "UnityEngine/Vector3.hpp"
-#include "UnityEngine/Quaternion.hpp"
+// bs-cordl generated headers for BS 1.40
+#include "GlobalNamespace/zzzz__MainCamera_def.hpp"
+#include "UnityEngine/zzzz__Camera_def.hpp"
+#include "UnityEngine/zzzz__Transform_def.hpp"
+#include "UnityEngine/zzzz__Vector3_def.hpp"
 
-#include "questui/shared/QuestUI.hpp"
-#include "questui/shared/BeatSaberUI.hpp"
+// BSML for settings UI
+#include "bsml/shared/BSML.hpp"
+#include "bsml/shared/BSML/Components/Settings/SliderSetting.hpp"
 
-#include "HMUI/ViewController.hpp"
-#include "UnityEngine/GameObject.hpp"
-#include "UnityEngine/RectTransform.hpp"
+#include "paper2_scotland2/shared/logger.hpp"
 
-#include <string>
-
-static ModInfo modInfo;
+static modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
 static CameraConfig config;
 
-Logger& getLogger() {
-    static Logger* logger = new Logger(modInfo);
-    return *logger;
+Paper::ConstLoggerContext<18ULL> static const& getLogger() {
+    static auto ctx = Paper::Logger::WithContext<MOD_ID>();
+    return ctx;
 }
 
 // ---- Settings UI ----
 
+#include "HMUI/ViewController.hpp"
+
 void DidActivate(HMUI::ViewController* self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
     if (!firstActivation) return;
 
-    auto container = QuestUI::BeatSaberUI::CreateScrollableSettingsContainer(self->get_transform());
-
-    // X Offset
-    QuestUI::BeatSaberUI::CreateSliderSetting(
-        container->get_transform(),
-        "X Offset", 0.01f, config.xOffset, -2.0f, 2.0f,
-        [](float val) {
-            config.xOffset = val;
-            SaveConfig(config);
-        }
-    );
-
-    // Y Offset
-    QuestUI::BeatSaberUI::CreateSliderSetting(
-        container->get_transform(),
-        "Y Offset", 0.01f, config.yOffset, -2.0f, 2.0f,
-        [](float val) {
-            config.yOffset = val;
-            SaveConfig(config);
-        }
-    );
-
-    // Z Offset
-    QuestUI::BeatSaberUI::CreateSliderSetting(
-        container->get_transform(),
-        "Z Offset", 0.01f, config.zOffset, -2.0f, 2.0f,
-        [](float val) {
-            config.zOffset = val;
-            SaveConfig(config);
-        }
-    );
-
-    // X Rotation
-    QuestUI::BeatSaberUI::CreateSliderSetting(
-        container->get_transform(),
-        "X Rotation", 1.0f, config.xRotation, -180.0f, 180.0f,
-        [](float val) {
-            config.xRotation = val;
-            SaveConfig(config);
-        }
-    );
-
-    // Y Rotation
-    QuestUI::BeatSaberUI::CreateSliderSetting(
-        container->get_transform(),
-        "Y Rotation", 1.0f, config.yRotation, -180.0f, 180.0f,
-        [](float val) {
-            config.yRotation = val;
-            SaveConfig(config);
-        }
-    );
-
-    // Z Rotation
-    QuestUI::BeatSaberUI::CreateSliderSetting(
-        container->get_transform(),
-        "Z Rotation", 1.0f, config.zRotation, -180.0f, 180.0f,
-        [](float val) {
-            config.zRotation = val;
-            SaveConfig(config);
-        }
-    );
+    BSML::parse_and_inject(self->get_transform(), R"(
+        <bg xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
+            xsi:noNamespaceSchemaLocation='https://raw.githubusercontent.com/nicoco007/BeatSaberMarkupLanguage/main/Schema/bsml-schema.xsd'>
+            <vertical child-control-height='false'>
+                <slider-setting text='X Offset' value='xOffset' min='-2' max='2' increment='0.01' apply-on-change='true'/>
+                <slider-setting text='Y Offset' value='yOffset' min='-2' max='2' increment='0.01' apply-on-change='true'/>
+                <slider-setting text='Z Offset' value='zOffset' min='-2' max='2' increment='0.01' apply-on-change='true'/>
+                <slider-setting text='X Rotation' value='xRotation' min='-180' max='180' increment='1' apply-on-change='true'/>
+                <slider-setting text='Y Rotation' value='yRotation' min='-180' max='180' increment='1' apply-on-change='true'/>
+                <slider-setting text='Z Rotation' value='zRotation' min='-180' max='180' increment='1' apply-on-change='true'/>
+            </vertical>
+        </bg>
+    )", nullptr);
 }
 
 // ---- Camera hook ----
@@ -108,13 +57,13 @@ MAKE_HOOK_MATCH(MainCamera_Awake, &GlobalNamespace::MainCamera::Awake, void, Glo
 
     auto* transform = cam->get_transform();
 
-    UnityEngine::Vector3 pos = transform->get_localPosition();
+    auto pos = transform->get_localPosition();
     pos.x += config.xOffset;
     pos.y += config.yOffset;
     pos.z += config.zOffset;
     transform->set_localPosition(pos);
 
-    UnityEngine::Vector3 rot = transform->get_localEulerAngles();
+    auto rot = transform->get_localEulerAngles();
     rot.x += config.xRotation;
     rot.y += config.yRotation;
     rot.z += config.zRotation;
@@ -123,22 +72,22 @@ MAKE_HOOK_MATCH(MainCamera_Awake, &GlobalNamespace::MainCamera::Awake, void, Glo
 
 // ---- Entry point ----
 
-extern "C" void setup(ModInfo& info) {
-    info.id      = "CameraOffsetMod";
-    info.version = "0.1.0";
-    modInfo      = info;
+extern "C" __attribute__((visibility("default"))) void setup(CModInfo* info) {
+    info->id      = MOD_ID;
+    info->version = VERSION;
+    info->version_long = 0;
     getLogger().info("Camera Offset Mod setup called");
 }
 
-extern "C" void load() {
+extern "C" __attribute__((visibility("default"))) void late_load() {
     il2cpp_functions::Init();
     config = LoadConfig();
 
     getLogger().info("Installing Camera Offset Mod hooks...");
     INSTALL_HOOK(getLogger(), MainCamera_Awake);
 
-    QuestUI::Init();
-    QuestUI::Register::RegisterModSettingsViewController(modInfo, DidActivate);
+    BSML::Init();
+    BSML::Register::RegisterSettingsMenu(modInfo, DidActivate);
 
     getLogger().info("Camera Offset Mod loaded successfully");
 }
